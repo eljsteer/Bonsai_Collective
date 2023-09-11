6f02ab13912e46f95b14ef12a9efa63f1b02f629
Jason <el.jsteer@gmail.com>
2023-09-11 18:44:26 +1000

    complete the routes and main entry and templates for pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom"
 import './App.css'
+import Home from './pages/Home';
+// import ErrorPage from "./utils/error-page.jsx"
+
+// Pages
+import Login from "./pages/Login";
+import Bonzai from "./pages/BonzaiTrees";
+import Signup from "./pages/Signup";
+
+import RootLayout from "./layouts/RootLayout"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="bonzai" element = {<Bonzai/>} />
+      <Route path="login" element = {<Login/>} />
+      <Route path="signup" element = {<Signup/>} />
+    </Route>
+  )
+)
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router}/>
     </>
   )
 }
