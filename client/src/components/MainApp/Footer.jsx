cfd1baebed88672379fd77d7ea49b30b97b8fcf2
Jason <el.jsteer@gmail.com>
2024-11-01 18:48:07 +1100

    update for smaller layouts

--- client/src/components/MainApp/Footer.jsx
+++ client/src/components/MainApp/Footer.jsx
@@ -1,4 +1,4 @@
-// import { Button}  from "@mui/material";
+import { Box, Typography}  from "@mui/material";
 // import { Link } from "react-router-dom"
 import { FaPinterest } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
@@ -11,81 +11,31 @@ import "./styles/footer.css"
 ////------------------------------////
 export default function Footer() {
   return (
-    <div className="footer">
-      <div className="res__footer section__padding">
-        {/* <div className="res__footer-links">
-          <div className="res__footer-links_div">
-            <h4>About</h4>
-            <Link className="res__footer-links_Link" href="/about">
-              <Button>About</Button>
-            </Link>
-            <Link className="res__footer-links_Link" href="/team">
-              <Button>Team</Button>
-            </Link>
-            <Link className="res__footer-links_Link" href="/testimonials">
-              <Button>Testimonials</Button>
-            </Link>
-          </div>
-          <div className="res__footer-links_div">
-            <h4>Explore</h4>
-            <Link className="res__footer-links_Link" href="/bonsais">
-              <Button>Bonsais</Button>
-            </Link>
-            <Link className="res__footer-links_Link" href="/growers">
-              <Button>Growers</Button>
-            </Link>
-            <Link className="res__footer-links_Link" href="/vintage">
-              <Button>Vintage</Button>
-            </Link>
-          </div>
-          <div className="res__footer-links_div">
-            <h4>Resources</h4>
-            <Link className="res__footer-links_Link" href="/Essentials">
-              <Button>Essentials</Button>
-            </Link>
-            <Link className="res__footer-links_Link" href="/decorations">
-              <Button>Decorations</Button>
-            </Link>
-            <Link className="res__footer-links_Link" href="/delivery">
-              <Button>Delivery</Button>
-            </Link>
-          </div>
-          <div className="res__footer-links_div">
-            <h4>Contact</h4>
-            <Link className="res__footer-links_Link" href="/contact">
-              <Button>Contact</Button>
-            </Link>
-            <Link className="res__footer-links_Link" href="/faqs">
-              <Button>FAQs</Button>
-            </Link>
-            <Link className="res__footer-links_Link" href="/blog">
-              <Button>Grow Blog</Button>
-            </Link>
-          </div>
-        </div> */}
+    <Box className="footer">
+      <Box className="res__footer section__padding">
       <hr />
-      <div className="res__footer-below">
-        <div className="res__footer-copyright">
-          <p>
+      <Box className="res__footer-below">
+        <Box className="res__footer-copyright">
+          <Typography>
             @{new Date().getFullYear()} Eljsteer. All rights reserved.
-          </p>
-          <div className="res__footer-below-links">
-            <a href="/terms"><div><p>Terms & Conditions</p></div></a>
-            <a href="/privacy"><div><p>Privacy</p></div></a>
-            <a href="/security"><div><p>Security</p></div></a>
-          </div>
-        </div>
-        <div className="res__footer-social_div" style={{alignItems: "center"}}>
+          </Typography>
+          <Box className="res__footer-below-links">
+            <a href="/about"><Box><Typography>About</Typography></Box></a>
+            <a href="/blog"><Box><Typography>Blog</Typography></Box></a>
+            <a href="/Terms"><Box><Typography>Terms</Typography></Box></a>
+          </Box>
+        </Box>
+        <Box className="res__footer-social" style={{alignItems: "center"}}>
           <h4>Coming soon on</h4>
-          <div className="socialmedia">
+          <Box id="socialmedia">
             <FaXTwitter style={{fontSize:"2rem", margin:"2.5px"}}/>
             <FaFacebook style={{fontSize:"2rem", margin:"2.5px"}}/>
             <FaPinterest style={{fontSize:"2rem", margin:"2.5px"}}/>
             <FaInstagram style={{fontSize:"2rem", margin:"2.5px"}}/>
-          </div>
-        </div>
-      </div>
-      </div>
-    </div>
+          </Box>
+        </Box>
+      </Box>
+      </Box>
+    </Box>
   )
 }
